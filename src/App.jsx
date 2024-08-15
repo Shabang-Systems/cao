2fd7a82f0052816a52c851cd1e31b17375c31ae6
Houjun Liu <houjun@jemoka.com>
2024-08-15 08:47:37 -0700

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
+// utiltiies
 import { useState, useEffect } from "react";
 import { appWindow } from "@tauri-apps/api/window";
+
+// routing
+import {
+    createBrowserRouter,
+    RouterProvider,
+    Route,
+    Link,
+} from "react-router-dom";
+
+// globals
 import { ThemeContext } from "./contexts.js";
+import store from "@api/store.js";
+import { Provider } from 'react-redux';
+
 import Capture from "@views/Capture.jsx";
 
 import "./theme.css";
 import "./app.css";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: (
+            <Capture/>
+        ),
+    },
+]);
 
 function App() {
     const [isDark, setIsDark] = useState(false);
@@ -23,16 +46,18 @@ function App() {
     }, []);
 
     return (
-        <ThemeContext.Provider value={{
-            dark: isDark
-        }}>
-            <div id="theme-box" className={isDark ? "dark" : ""}>
-                <div className={"global w-screen h-screen"}>
-                    <div id="top-hide"></div>
-                    <Capture/>
+        <Provider store={store}>
+            <ThemeContext.Provider value={{
+                dark: isDark
+            }}>
+                <div id="theme-box" className={isDark ? "dark" : ""}>
+                    <div className={"global w-screen h-screen"}>
+                        <div id="top-hide"></div>
+                        <RouterProvider router={router}/>
+                    </div>
                 </div>
-            </div>
-        </ThemeContext.Provider>
+            </ThemeContext.Provider>
+        </Provider>
     );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -4,14 +4,9 @@ import App from "./app";
 import "./index.css";
 import { appWindow } from "@tauri-apps/api/window";
 
-import store from "@api/store.js";
-import { Provider } from 'react-redux';
-
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <Provider store={store}>
             <App />
-        </Provider>
-    </React.StrictMode>,
+    </React.StrictMode>
 );
