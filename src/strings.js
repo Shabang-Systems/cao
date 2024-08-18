196665f234b89e4f8b0179b501744bfd23c4b1f0
Houjun Liu <houjun@jemoka.com>
2024-08-17 22:58:44 -0700

    more tooltips refactoring

--- src/strings.js
+++ src/strings.js
@@ -1,5 +1,13 @@
 export default {
     COMPONENTS__EDITOR__CM_PLACEHOLDER: "Go on, capture something...",
-    COMPONENTS__EDITOR__PARAGRAPH_HINT: "Tap between paragraphs to seperate into subtasks."
+    COMPONENTS__EDITOR__PARAGRAPH_HINT: "Tap between paragraphs to seperate into subtasks.",
+    TOOLTIPS: {
+        CAPTURE: "Capture",
+        BROWSE: "Browse",
+        SUBMIT: "Submit",
+        CANCEL: "Cancel",
+        PREVIOUS_SHEET: "Previous Sheet",
+        NEXT_SHEET: "Next Sheet",
+    }
 }
 
