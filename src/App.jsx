9c1398adafecb4008810814fbc91229a0981b11e
Houjun Liu <houjun@jemoka.com>
2024-08-17 22:53:20 -0700

    tooltips!

--- src/App.jsx
+++ src/App.jsx
@@ -22,6 +22,7 @@ import Capture from "@views/Capture.jsx";
 import Browser from "@views/Browser.jsx";
 
 //// components ////
+import { Tooltip } from 'react-tooltip';
 import { ErrorBoundary, GlobalErrorModal } from "@components/error.jsx";
 
 //// styles ////
@@ -33,6 +34,7 @@ function RoutableMain() {
 
     return (
         <div id="routable-main">
+            <Tooltip id="rootp" />
             <div className="bottom-nav absolute" style={{bottom: "10px", left: "10px",
                                                          zIndex: 20000}}>
                 <Link to={"/"}>
@@ -40,12 +42,12 @@ function RoutableMain() {
                         <i className="fa-solid fa-person-running"></i>
                     </div>
                 </Link>
-                <Link to={"/"}>
+                <Link to={"/"} data-tooltip-id="rootp" data-tooltip-content="Capture">
                     <div className={"bottom-nav-button"+(loc.pathname == "/" ? " active" : "")}>
                         <i className="fa-solid fa-inbox"></i>
                     </div>
                 </Link>
-                <Link to={"/browse"}>
+                <Link to={"/browse"} data-tooltip-id="rootp" data-tooltip-content="Browse">
                     <div className={"bottom-nav-button"+(loc.pathname == "/browse" ? " active" : "")}>
                         <i className="fa-solid fa-layer-group"></i>
                     </div>
