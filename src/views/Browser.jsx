b2569ce6c14b3b28cf60f2a0be87b16d95225351
Houjun Liu <houjun@jemoka.com>
2024-08-20 21:45:55 -0700

    commiting before backend actually refactors

--- src/views/Browser.jsx
+++ src/views/Browser.jsx
@@ -3,13 +3,22 @@ import { allIncompleteTasksSelector } from '@api/tasks.js';
 
 import Task from "@components/task.jsx";
 
+import "./Browser.css";
+
 export default function Browser() {
     let entries = useSelector(allIncompleteTasksSelector);
 
     return (
         <div className="m-12">
+            <div className="capture-cm">
+                <input className="capture-cm-box" placeholder="you can filter by tags @likeso" autocorrect="off" autoCorrect={false} />
+            </div>
+
             {entries.map((x) => (
+                <>
                 <Task key={x.id} task={x}/>
+                <div className="task-divider focused cursor-default"><div className="task-divider-line"></div></div>
+                </>
             ))}
         </div>
     );
