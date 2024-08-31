82bb87f44ad8acba006d463362371ff293178efe
Houjun Liu <houjun@jemoka.com>
2024-08-31 16:59:27 -0700

    multiple pads

--- src/views/Browser.jsx
+++ src/views/Browser.jsx
@@ -10,22 +10,31 @@ import strings from "@strings";
 import { set, pop, grow, view } from "@api/browse.js";
 
 import "./Browser.css";
+import "./Capture.css";
 import { useEffect, useState, useCallback } from 'react';
 
 export default function Browser() {
     let entries = useSelector((s) => s.tasks.entries);
     const dispatch = useDispatch();
     const isLoading = useSelector((s) => s.tasks.loading);
-    const q = useSelector(createSelector(
+    const currentQuery = useSelector(createSelector(
         [(state) => state.browse.searches[state.browse.current]],
         (res) => res?res:{},
         {devModeChecks: {identityFunctionCheck: 'never'}}
     ));
+    const length = useSelector((state) =>
+        state.browse.searches.length
+    );
+    const captureID = useSelector((state) =>
+        state.browse.current
+    );
+
     const [searchValue, setSearchValue] = useState("");
 
     useEffect(() => {
-        dispatch(query(q));
-    }, [q]);
+        setSearchValue(currentQuery.query_regexp ? currentQuery.query_regexp : "");
+        dispatch(query(currentQuery));
+    }, [currentQuery]);
 
     const executeQuery = useCallback((e) => {
         let text = e.replace(/[“”]/g, '"').replace(/[‘’]/g, "'");
@@ -39,7 +48,7 @@ export default function Browser() {
         }
         
         let newQuery = {
-            ...q,
+            ...currentQuery,
             query_regexp: rest,
             tags
         };
@@ -48,19 +57,19 @@ export default function Browser() {
         setSearchValue(text);
     });
 
-    const [availability, setAvailbility] = useState(q.availability ? q.availability : "Incomplete");
+    const [availability, setAvailbility] = useState(currentQuery.availability ? currentQuery.availability : "Incomplete");
     useEffect(() => {
         let newQuery = {
-            ...q,
+            ...currentQuery,
             availability
         };
         dispatch(set(newQuery));
     }, [availability]);
-    const [order, setOrder] = useState(q.order && q.order.order ? q.order.order : "Captured");
-    const [ascending, setAscending] = useState(q.order && q.order.ascending ? q.order.ascending : false);
+    const [order, setOrder] = useState(currentQuery.order && currentQuery.order.order ? currentQuery.order.order : "Captured");
+    const [ascending, setAscending] = useState(currentQuery.order && currentQuery.order.ascending ? currentQuery.order.ascending : false);
     useEffect(() => {
         let newQuery = {
-            ...q,
+            ...currentQuery,
             order: { order, ascending }
         };
         dispatch(set(newQuery));
@@ -73,65 +82,107 @@ export default function Browser() {
 
     return (
         <div className="m-12">
-            <div className="capture-cm">
-                <input
-                    value={searchValue}
-                    onChange={(e) => executeQuery(e.target.value)}
-                    className="capture-cm-box"
-                    placeholder={strings.VIEWS__BROWSER}
-                    autoCorrect="off" />
-            </div>
-            <div className="capture-options">
-                <div className="capture-options-wrapper">
-                    <i className="fa-solid fa-filter" />
-                    <select className="capture-options-select"
-                            value={availability}
-                            onChange={(v) => setAvailbility(v.target.value)}>
-                        <option className="capture-options-option" value="Incomplete">Incomplete</option>
-                        <option className="capture-options-option" value="Available">Available</option>
-                        <option className="capture-options-option" value="Done">Completed</option>
-                        <option className="capture-options-option" value="All">All Tasks</option>
-                    </select>
+            <div className="mr-8">
+                <div className="capture-cm">
+                    <input
+                        value={searchValue}
+                        onChange={(e) => executeQuery(e.target.value)}
+                        className="capture-cm-box"
+                        placeholder={strings.VIEWS__BROWSER}
+                        autoCorrect="off" />
                 </div>
-                <div className="capture-options-wrapper">
-                    <i className="fa-solid fa-arrow-down-short-wide" />
-                    <select
-                        value={order}
-                        onChange={(v) => setOrder(v.target.value)}
-                        className="capture-options-select">
-                        <option className="capture-options-option" value="Due">Due Date</option>
-                        <option className="capture-options-option" value="Start">Start Date</option>
-                        <option className="capture-options-option" value="Captured">Creation</option>
-                        <option className="capture-options-option" value="Scheduled">Scheduled</option>
-                    </select>
-                </div>
-                <div className="capture-options-wrapper">
-                    <i className="fa-solid fa-sort" />
-                    <select
-                        onChange={(v) => setAscending(v.target.value == "ascending")}
-                        value={ascending ? "ascending" : "descending"}
-                        className="capture-options-select">
-                        <option className="capture-options-option" value="descending">Descending</option>
-                        <option className="capture-options-option" value="ascending">Ascending</option>
-                    </select>
-                </div>
-            </div>
-            <div>
-                <div className="task-divider" onClick={() => {
-                    setJustAbtibd(true);
-                    dispatch(abtib([""]));
-                }}><div className="task-divider-line"></div></div>
-                {entries.map((x, indx) => (
-                    <div key={x.id}>
-                        <Task
-                            task={x}
-                            initialFocus={justAbtibd && indx == 0}
-                            onFocusChange={(x) => {if (!x) setJustAbtibd(false);}}
-                        />
-                        <div className="task-divider focused cursor-default"><div className="task-divider-line"></div></div>
+                <div className="capture-options">
+                    <div className="capture-options-wrapper">
+                        <i className="fa-solid fa-filter" />
+                        <select className="capture-options-select"
+                                value={availability}
+                                onChange={(v) => setAvailbility(v.target.value)}>
+                            <option className="capture-options-option" value="Incomplete">Incomplete</option>
+                            <option className="capture-options-option" value="Available">Available</option>
+                            <option className="capture-options-option" value="Done">Completed</option>
+                            <option className="capture-options-option" value="All">All Tasks</option>
+                        </select>
                     </div>
-                ))}
+                    <div className="capture-options-wrapper">
+                        <i className="fa-solid fa-arrow-down-short-wide" />
+                        <select
+                            value={order}
+                            onChange={(v) => setOrder(v.target.value)}
+                            className="capture-options-select">
+                            <option className="capture-options-option" value="Due">Due Date</option>
+                            <option className="capture-options-option" value="Start">Start Date</option>
+                            <option className="capture-options-option" value="Captured">Creation</option>
+                            <option className="capture-options-option" value="Scheduled">Scheduled</option>
+                        </select>
+                    </div>
+                    <div className="capture-options-wrapper">
+                        <i className="fa-solid fa-sort" />
+                        <select
+                            onChange={(v) => setAscending(v.target.value == "ascending")}
+                            value={ascending ? "ascending" : "descending"}
+                            className="capture-options-select">
+                            <option className="capture-options-option" value="descending">Descending</option>
+                            <option className="capture-options-option" value="ascending">Ascending</option>
+                        </select>
+                    </div>
+                </div>
+                <div>
+                    <div className="task-divider" onClick={() => {
+                        setJustAbtibd(true);
+                        dispatch(abtib([""]));
+                    }}><div className="task-divider-line"></div></div>
+                    {entries.map((x, indx) => (
+                        <div key={x.id}>
+                            <Task
+                                task={x}
+                                initialFocus={justAbtibd && indx == 0}
+                                onFocusChange={(x) => {if (!x) setJustAbtibd(false);}}
+                            />
+                            <div className="task-divider focused cursor-default"><div className="task-divider-line"></div></div>
+                        </div>
+                    ))}
+                </div>
             </div>
+            <div className="absolute captureid-outer" style={{top: "10px", right: "10px",
+                                                              zIndex: 20000,
+                                                              paddingTop: 20, paddingRight: 5}}>
+                <div className="mb-3">
+                    <div className="button" onClick={() => {
+                        if (!currentQuery.query_regexp) {
+                            dispatch(pop());
+                        } else {
+                            dispatch(view(captureID-1));
+                        }
+                    }} data-tooltip-id="rootp" data-tooltip-content={strings.TOOLTIPS.PREVIOUS_SHEET} data-tooltip-place="left">
+                        <i className="fa-solid fa-chevron-up"></i>
+                    </div>
+                    <div className="button" onClick={() => {
+                        if (captureID == length - 1 ) {
+                            dispatch(grow());
+                        } else {
+                            dispatch(view(captureID+1));
+                        }
+                    }} data-tooltip-id="rootp"  data-tooltip-content={strings.TOOLTIPS.NEXT_SHEET} data-tooltip-place="left">
+                        <i className="fa-solid fa-chevron-down"></i>
+                    </div>
+
+
+                </div>
+                <ul className="captureid-wrapper">
+                    {
+                        [...Array(length).keys()].map((x) => {
+                            return (
+                                <li key={x} className={"captureid-dot " + (
+                                    x == captureID ? "active" : ""
+                                )}></li>
+                            );
+                        })
+                    }
+                </ul> 
+
+            </div>
+
+
         </div>
     );
 }
