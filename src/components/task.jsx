1605e75e61217a0b09dcd7d2c01a0d4006808e46
Houjun Liu <houjun@jemoka.com>
2024-09-10 19:52:42 -0700

    make clicking on the button actually do a thing

--- src/components/task.jsx
+++ src/components/task.jsx
@@ -108,7 +108,8 @@ export default function Task( { task, initialFocus, onFocusChange } ) {
                 onClose={() => setDeferOpen(false)}
                 ref={deferRef} />
 
-            <div className={`task-action floating-task-action ${hasFocus ? "opacity-1" : "opacity-0" } group-hover:opacity-100 transition-opacity` }
+            <div className={`task-action cursor-pointer floating-task-action ${hasFocus ? "opacity-1" : "opacity-0" } group-hover:opacity-100 transition-opacity` }
+                 style={{cursor: "pointer !important", zIndex: 100000}}
                     onClick={() => {
                         // TODO completing tasks is a bit of a thing so
                         // TODO supporting repeating tasks, etc.
